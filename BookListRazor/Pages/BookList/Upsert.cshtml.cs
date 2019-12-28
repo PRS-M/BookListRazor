ce2f7103b30016d31a537dd353c3101ac3242ee0
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2019-12-28 02:26:58 +0100

    Update-Insert (Upsert) View added.

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // Create
+                return Page();
+            }
+
+            // Update
+            Book = await this.db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (Book.Id == 0)
+                {
+                    this.db.Book.Add(Book);
+                }
+                else
+                {
+                    this.db.Book.Update(Book);
+                }
+
+                await this.db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
